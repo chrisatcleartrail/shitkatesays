b2eb04169308a7392be337d570f32447b618d762
chrisatcleartrail <chris@cleartrail.net>
2025-08-11 19:13:53 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <h1>Shit Kate Says</h1>
+      <p>Welcome to your React + TypeScript site deployed via GitHub Pages!</p>
+      <p>Edit <code>src/App.tsx</code> to get started.</p>
+    </main>
+  );
+}
+
+export default App;
